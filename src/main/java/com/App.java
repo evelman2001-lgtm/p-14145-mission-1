bd192a0e554274cc6184d4e4eee87aef1f1e8924
양상훈 <evelman2001@gmail.com>
2026-05-05 23:02:30 +0900

    rq 클래스 구현 및 리팩토링

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class App {
-    private Scanner scanner = new Scanner(System.in);
+    public Scanner scanner = new Scanner(System.in);
 
     private List<WiseSaying> wiseSayings = new ArrayList<>();
     private int lastId = 0;
@@ -16,24 +17,31 @@ public class App {
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
+            Rq rq = new Rq(cmd);
 
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("목록")) {
-                actionList();
-            } else if (cmd.equals("등록")) {
-                actionWrite();
-            } else if (cmd.startsWith("삭제")) {
-                actionDelete(cmd); // cmd에 담아서 데이터 전달 (Parameter)
-            } else if (cmd.startsWith("수정")) {
-                actionModify(cmd); // "삭제와 마찬가지로 cmd에 담아서 데이터 전달
+            switch (rq.getActionName()) {
+                case "종료":
+                    System.out.println("명언 앱을 종료합니다.");
+                return;
+                case "목록":
+                    actionList();
+                    break;
+                case "등록":
+                    actionWrite();
+                    break;
+                case "삭제":
+                    actionDelete(rq);
+                    break;
+                case "수정":
+                   actionModify(rq);
+                    break;
+                default:
+                    System.out.println("존재하지 않는 명령입니다.");
             }
         }
-
-        scanner.close();
     }
 
-   public void actionWrite() {
+    private void actionWrite() {
         System.out.println("명언 :");
         String content = scanner.nextLine().trim();
         System.out.println("작가 :");
@@ -41,23 +49,22 @@ public class App {
 
         WiseSaying wiseSaying = write(content, author);
 
-        System.out.println("%d번 명언이 등록 되었습니다.".formatted(wiseSaying.id));
+        System.out.println("%d번 명언이 등록 되었습니다.".formatted(wiseSaying.getId()));
     }
 
-    public void actionDelete(String cmd) {
-        if (!cmd.contains("?")) {
+    private void actionDelete(Rq rq) {
+
+        int id = rq.getParamAsInt("id", -1);
+        if (id == -1) {
             System.out.println("삭제 번호를 정확히 입력해주세요.");
             return;
         }
-        String[] cmdBits = cmd.split("\\?", 2);
-        String[] paramBits = cmdBits[1].split("=", 2);
-        int id = Integer.parseInt(paramBits[1]);
 
         boolean found = false;
 
         for (int i = 0; i < wiseSayings.size(); i++) {
             WiseSaying wiseSaying = wiseSayings.get(i);
-            if (wiseSaying.id == id) {
+            if (wiseSaying.getId() == id) {
                 wiseSayings.remove(i);
                 found = true;
                 System.out.println("%d번 명언이 삭제되었습니다\n".formatted(id));
@@ -68,35 +75,33 @@ public class App {
             System.out.println("%d번 명언은 존재하지 않습니다\n".formatted(id));
         }
     }
-    public void actionModify (String cmd) {
-        if (!cmd.contains("?")) {
+    private void actionModify (Rq rq) {
+        int id = rq.getParamAsInt("id", -1);
+        if (id == -1) {
             System.out.println("수정 번호를 정확히 입력해주세요.");
-            return;
+        return;
         }
-        String[] cmdBits = cmd.split("\\?", 2);
-        String[] paramBits = cmdBits[1].split("=", 2);
-        int id = Integer.parseInt(paramBits[1]);
 
         boolean found = false;
 
         for (int i = wiseSayings.size() -1 ; i >= 0; i--) {
 
             WiseSaying wiseSaying = wiseSayings.get(i);
-            if (wiseSaying.id ==id) {
+            if (wiseSaying.getId() ==id) {
                 found = true;
 
-                System.out.println("명언(기존) : %s".formatted(wiseSaying.content));
+                System.out.println("명언(기존) : %s".formatted(wiseSaying.getContent()));
                 System.out.println("명언 :");
                 String content = scanner.nextLine();
 
-                System.out.println("작가(기존) :%s".formatted(wiseSaying.author));
+                System.out.println("작가(기존) :%s".formatted(wiseSaying.getAuthor()));
                 System.out.println("작가 :");
                 String author = scanner.nextLine();
 
-                wiseSaying.content = content;
-                wiseSaying.author = author;
+                wiseSaying.setContent(content);
+                wiseSaying.setAuthor(author);
 
-                System.out.println("%d번 명언이 수정되었습니다.".formatted(wiseSaying.id));
+                System.out.println("%d번 명언이 수정되었습니다.".formatted(wiseSaying.getId()));
                 break;
             }
         }
@@ -106,25 +111,25 @@ public class App {
     }
 
 
-    public WiseSaying write(String content, String author) {
+    private WiseSaying write(String content, String author) {
         WiseSaying wiseSaying = new WiseSaying();
-        wiseSaying.id = ++lastId;
-        wiseSaying.content = content;
-        wiseSaying.author = author;
+        wiseSaying.getId();
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
 
         wiseSayings.add(wiseSaying);
 
         return wiseSaying;
     }
 
-    public void actionList() {
+    private void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
         for (int i = wiseSayings.size() - 1; i >= 0; i--) {
             WiseSaying wiseSaying = wiseSayings.get(i);
 
-            System.out.println("%d / %s / %s ".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+            System.out.println("%d / %s / %s ".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
         }
     }
 }
